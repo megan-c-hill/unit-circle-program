aae1453c246c108c6bb0d5c3d06180abdf177e0a
Megan Hill <meganhill.iowa@gmail.com>
2018-12-19 19:02:32 -0600

    init commit

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../styles/Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <header className="home-header">
+          <p>
+            Home Page
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
